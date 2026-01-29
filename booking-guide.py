7f37c9f03f13da710228b969115df6b6c8a14d7e
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-29 12:46:14 +0000

    Cleaned up some syntax bugs

--- booking-guide.py
+++ booking-guide.py
@@ -1,10 +1,40 @@
 class Transaction:
-    pass
+    def __init__(self, custName, price, quantity):
+        self.custName = name
+        self.price = price
+        self.quantity = quantity
 
-class Sale:
-    pass
+    def printSummary():
+        pass
 
-class Return:
-    pass
+    def calculateTotal():
+        pass
+
+
+class Sale(transaction):
+    def __init__(self, custName, price, quantity, eventName):
+        super().__init__(custName, price, quantity)
+
+        self.eventName
+
+    def calculateTotal():
+        pass
+
+    def printSummary():
+        pass
+    
+
+class Return(Transaction):
+    def __init__(self, custName, price, quantity, returnDate):
+        super().__init__(custName, price, quantity)
+
+        self.returnDate = returnDate
+
+    def calculateTotal():
+        pass
+
+    def printSummary():
+        pass
+
 
 main():
