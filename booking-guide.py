f0d9fdd121b48b8e9ea1361eed28832588e18200
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-29 13:16:16 +0000

    added calc total and print summary for sale and return class

--- booking-guide.py
+++ booking-guide.py
@@ -1,27 +1,39 @@
-class Transaction:
+from abc import ABC, abstractmethod
+
+class Transaction(ABC):
     def __init__(self, custName, price, quantity):
-        self.custName = name
+        self.custName = custName
         self.price = price
         self.quantity = quantity
 
-    def printSummary():
+    @abstractmethod
+    def printSummary(self):
         pass
 
-    def calculateTotal():
+    @abstractmethod
+    def calcTotal(self):
         pass
 
 
-class Sale(transaction):
+class Sale(Transaction):
     def __init__(self, custName, price, quantity, eventName):
         super().__init__(custName, price, quantity)
 
-        self.eventName
+        self.eventName = eventName
 
-    def calculateTotal():
-        pass
+    def calcTotal(self):
+        total = (self.price * self.quantity) * 1.005
+        return total
 
-    def printSummary():
-        pass
+    def printSummary(self):
+        total = self.calcTotal()
+        print("\n=== Sale Summary ===")
+        print(f"Customer name: {self.custName}")
+        print(f"Event name:  {self.eventName}")
+        print(f"Quantity: {self.quantity}")
+        print(f"Price: {self.price}")
+        print(f"Total: {self.total:.2f}")
+        print("============================\n")
     
 
 class Return(Transaction):
@@ -30,11 +42,26 @@ class Return(Transaction):
 
         self.returnDate = returnDate
 
-    def calculateTotal():
-        pass
+    def calcTotal(self):
+        total = (self.price * self.quantity)
+        return total
 
-    def printSummary():
-        pass
+    def printSummary(self):
+        total = self.calcTotal()
+        print("\n=== Return Summary ===")
+        print(f"Customer name: {self.custName}")
+        print(f"Event name:  {self.eventName}")
+        print(f"Quantity: {self.quantity}")
+        print(f"Price: {self.price}")
+        print(f"Return date: {self.returnDate}")
+        print(f"Total: {self.total:.2f}")
+        print("============================\n")
 
 
-main():
+def main():
+    
+    
+
+
+if __name__ == "__main__":
+    main()
